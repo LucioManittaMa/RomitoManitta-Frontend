97997d0062760033214ae8e6640bb7e968a24497
LucioManittaMa <mazzaresilu@gmail.com>
2024-04-14 18:08:51 -0300

    Mejorando el main de la pagina principal

---
+++ script.js
@@ -0,0 +1,17 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var slides = document.getElementsByClassName("slide");
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Cambia la imagen cada 5 segundos (5000 milisegundos)
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
