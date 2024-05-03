fcd51afe0dbbddda17c41c4143af41cbb7b33e7e
LucioManittaMa <mazzaresilu@gmail.com>
2024-05-03 13:03:21 -0300

    Agregando eventos

---
+++ links.js
@@ -0,0 +1,13 @@
+const logoutButton = document.getElementById("logout-button");
+const loginButton = document.getElementById("login-button");
+const singinButton = document.getElementById("signin-button");
+
+logoutButton.addEventListener("click", function() {
+  window.location.href = "./login.html";
+})
+loginButton.addEventListener("click", function(){
+    window.location.href = "./login.html"
+})
+singinButton.addEventListener("click", function(){
+    window.location.href = "./login.html"
+})
